3586cd9c6be72131898917d098b8db1b3a9235b9
monijuan <1019646292@qq.com>
2020-12-05 15:39:36 +0800

    first

---
+++ Assets/Script/GM.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GM : MonoBehaviour
+{
+    private bool isGameOver = false;
+
+    public Spawner spawner;
+    public Rotator rotator;
+
+    public void Start() { }
+    public void GameOver()
+    {
+        if(!isGameOver)
+        {
+            isGameOver = true;
+            Debug.Log("Game Over");
+            spawner.enabled = false;
+            rotator.enabled = false;
+            GetComponent<Animator>().SetTrigger("GameOver");
+        }
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    
+}
---
+++ Assets/Script/Pin.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour
+{
+    public float speed = 20f;
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = Vector2.up * speed;
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag=="Rotator")
+        {
+            rb.velocity = Vector2.zero;
+            transform.SetParent(col.transform);
+            Score.scoreValue++;
+            if(Random.Range(0f,1f)>0.5f)
+            {
+                col.GetComponent<Rotator>().speed += 15;
+                col.GetComponent<Rotator>().speed *= -1;
+            }
+        }
+        else if(col.tag=="Pin")
+        {
+            GameObject.FindObjectOfType<GM>().GameOver();
+        }
+    }
+}
